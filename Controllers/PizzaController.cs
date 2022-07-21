f4f4be68586d4f81425164cc4ec99373cde9bda9
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-07-21 16:46:58 +0200

    Edit GET and POST n to n | done

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -117,7 +117,8 @@ namespace la_mia_pizzeria_static.Controllers
         {
             using(PizzaContext db = new PizzaContext())
             {
-                Pizza mod = db.Pizza.Where(pizza => pizza.Id == id).FirstOrDefault();
+                Pizza mod = db.Pizza.Where(pizza => pizza.Id == id)
+                    .Include(pizza => pizza.ingredients).FirstOrDefault();
 
                 if(mod == null)
                 {
@@ -127,8 +128,25 @@ namespace la_mia_pizzeria_static.Controllers
                 PizzaCategories model = new PizzaCategories();
                 List<Category> categories = db.Categories.ToList();
 
+                List<SelectListItem> ingredientsList = new List<SelectListItem>();
+                List<Ingrediente> ingredients = db.Ingredienti.ToList();
+
+                foreach (Ingrediente i in ingredients)
+                {
+                    ingredientsList.Add(new SelectListItem() { Text = i.Name, Value = i.Id.ToString() });
+                }
+
+                model.Ingredients = ingredientsList;
+
                 model.Categories = categories;
                 model.Pizz = mod;
+
+                model.SelectedIngredients = new List<string>();
+
+                foreach(Ingrediente i in mod.ingredients)
+                {
+                    model.SelectedIngredients.Add(i.Id.ToString());
+                }
 
                 return View(model);
             }
@@ -141,12 +159,28 @@ namespace la_mia_pizzeria_static.Controllers
         {
             if (!ModelState.IsValid)
             {
+                using(PizzaContext db = new PizzaContext())
+                {
+                    List<Category> categories = db.Categories.ToList();
+                    model.Categories = categories;
+
+                    List<SelectListItem> ingredientsList = new List<SelectListItem>();
+                    List<Ingrediente> ingredients = db.Ingredienti.ToList();
+
+                    foreach(Ingrediente i in ingredients)
+                    {
+                        ingredientsList.Add(new SelectListItem { Text = i.Name, Value = i.Id.ToString() });
+                    }
+
+                    model.Ingredients = ingredientsList;
+                }
                 return View(model);
             }
 
             using(PizzaContext db = new PizzaContext())
             {
-                Pizza mod = db.Pizza.Where(pizza => pizza.Id == id).FirstOrDefault();
+                Pizza mod = db.Pizza.Where(pizza => pizza.Id == id)
+                    .Include(pizza => pizza.ingredients).FirstOrDefault();
 
                 if(mod != null)
                 {
@@ -154,8 +188,22 @@ namespace la_mia_pizzeria_static.Controllers
                     mod.Image = model.Pizz.Image;
                     mod.Description = model.Pizz.Description;
                     mod.Price = model.Pizz.Price;
-                    mod.ingredients = model.Pizz.ingredients;
+                    //mod.ingredients = model.Pizz.ingredients;
                     mod.CategoryId = model.Pizz.CategoryId;
+
+                    mod.ingredients.Clear();
+
+                    if(model.SelectedIngredients != null)
+                    {
+                        foreach (string selectedIngredientId in model.SelectedIngredients)
+                        {
+                            int selectedId = int.Parse(selectedIngredientId);
+
+                            Ingrediente ingrediente = db.Ingredienti.Where(i => i.Id == selectedId).FirstOrDefault();
+
+                            mod.ingredients.Add(ingrediente);
+                        }
+                    }
 
                     db.Update(mod);
 
